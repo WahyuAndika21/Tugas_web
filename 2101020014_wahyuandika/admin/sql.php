2f25812dca43f1c5c18687f38c8bebebd05ac3b7
WahyuAndika21 <97567315+WahyuAndika21@users.noreply.github.com>
2023-11-20 09:58:39 +0800

    Add files via upload

---
+++ 2101020014_wahyuandika/admin/sql.php
@@ -0,0 +1,10 @@
+<?php
+    $server     ="localhost";
+    $user       ="root";
+    $password   ="";
+    $database   ="2101020014_i putu wahyu andika";
+    
+    $con = mysqli_connect($server,$user,$password,$database);
+    
+    
+?>
